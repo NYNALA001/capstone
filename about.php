5ffded9fb25e65cff7662281fffde2d9bcacb723
Cameron Adams <camgadams@gmail.com>
2018-09-04 11:45:02 +0200

    add about.php file

---
+++ about.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>People</title>
+<?php include 'assets/php/dependencies.php'?>
+<?php include 'assets/php/server.php'?>
+</head>
+<body>
+	<?php include './assets/php/header.php';?>
+
+	The Centre for Artificial Intelligence Research (CAIR) is a South
+	African distributed Centre of Excellence that conducts foundational,
+	directed and applied research into various aspects of Artificial
+	Intelligence. CAIR has nodes at five South African universities: the
+	University of Cape Town, University of KwaZulu-Natal, North-West
+	University, University of Pretoria and Stellenbosch University, and is
+	coordinated and managed by the Meraka Institute at the Council for
+	Scientific and Industrial Research. CAIR was founded by Prof. Tommie
+	Meyer and Dr. Deshen Moodley in November 2011 as a joint research
+	centre between the University of KwaZulu-Natal and the Council for
+	Scientific and Industrial Research (CSIR). In 2015 CAIR expanded to
+	four other South African universities with the CSIR playing a
+	coordinating role. CAIR is primarily funded by the Department of
+	Science and Technology (DST), as part of the implementation of South
+	<br>
+	<br> Africa's ICT Research, Development and Innovation (RDI) Roadmap.
+	CAIR's mandate is to strengthen RDI capacity at participating nodes to
+	guide South Africa to a state of digital advantage in Artificial
+	Intelligence that will strengthen economic competitiveness and enable
+	an enhanced quality of life for all South Africans. The CSIR Meraka
+	Institute, as an implementing body of the RDI, established CAIR as a
+	National Centre of Excellence in Artificial Intelligence Research.
+
+	<?php include './assets/php/footer.php';?>
+
+</body>
+</html>
